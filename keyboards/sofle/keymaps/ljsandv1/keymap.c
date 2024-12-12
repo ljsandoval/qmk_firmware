ac90ce92521b81e4b4e93fec14125992a5a43629
Lucas Sandoval <lucas.j.sandoval@gmail.com>
2024-12-12 00:00:11 -0800

    adding mousekeys to nav layer

--- keyboards/sofle/keymaps/ljsandv1/keymap.c
+++ keyboards/sofle/keymaps/ljsandv1/keymap.c
@@ -42,8 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,     KC_1,    KC_2,    KC_3,      KC_4,          KC_5,                                     KC_6,       KC_7,       KC_8,      KC_9,      KC_0,   KC_BSPC,
   KC_TAB,     KC_Q,    KC_W,    KC_E,      KC_R,          KC_T,                                     KC_Y,       KC_U,       KC_I,      KC_O,      KC_P,   KC_BSLS,
   KC_LSFT,    KC_A,    KC_S,    KC_D,      KC_F,          KC_G,                                     KC_H,       KC_J,       KC_K,      KC_L,   KC_SCLN,   KC_QUOT,
-  KC_ESC,     KC_Z,    KC_X,    KC_C,      KC_V,          KC_B,    KC_MUTE,
-        XXXXXXX,     KC_N,       KC_M,    KC_COMM,    KC_DOT,   KC_SLSH,   KC_RSFT,
+  KC_ESC,     KC_Z,    KC_X,    KC_C,      KC_V,          KC_B,    KC_MUTE,             XXXXXXX,    KC_N,       KC_M,    KC_COMM,    KC_DOT,   KC_SLSH,   KC_RSFT,
               KC_LCTL, KC_GUI, KC_LALT, LT(_SYMNUM, KC_SPC), LT(_NAV, KC_BSPC),     LT(_NAV, KC_ENTER),  LT(_SYMNUM, KC_SPC),  KC_RALT,    KC_RGUI,  KC_RCTL
 ),
 //  TODO: Add KeyOveride for Bspc to use shift+backspace as delete
@@ -96,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------.                     ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |  Del |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Esc  |      |      |      |      |      |                    | PWrd | NWrd |      |      |      | DLine|
+ * | Esc  |      |      |  mup |      |      |                    | PWrd | NWrd |      |      |      | DLine|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |      |      |      |      |      |-------.    ,-------| Left | Down | Up   | Rigth|      |      |
  * |------+------+------+------+------+------| PAUSE |    |       |------+------+------+------+------+------|
@@ -108,11 +107,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 //  TODO: Rework parts of this to make it work across mac and win, may require more custom keycodes
 [_NAV] = LAYOUT(
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,     KC_DEL,
-  _______,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,                             KC_PRVWD, KC_NXTWD, XXXXXXX,   XXXXXX, XXXXXXX, C(KC_BSPC),
-  _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,                                KC_LEFT,  KC_DOWN,   KC_UP,  KC_RGHT, XXXXXXX,    XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MEDIA_PLAY_PAUSE,    _______, KC_LSTRT,  KC_LEND, XXXXXXX,  XXXXXXX, XXXXXXX,    XXXXXXX,
-                         _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______
+  _______, XXXXXXX, MS_ACL0, MS_ACL1, MS_ACL2, XXXXXXX,                         XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,     KC_DEL,
+  _______, XXXXXXX, MS_BTN1,   MS_UP, MS_BTN2, MS_WHLU,                        KC_PRVWD, KC_NXTWD, XXXXXXX,   XXXXXX, XXXXXXX, C(KC_BSPC),
+  _______, XXXXXXX, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD,                         KC_LEFT,  KC_DOWN,   KC_UP,  KC_RGHT, XXXXXXX,    XXXXXXX,
+  _______, XXXXXXX, MS_WHLL, XXXXXXX, MS_WHLR, XXXXXXX, KC_MPLY,      _______, KC_LSTRT,  KC_LEND, XXXXXXX,  XXXXXXX, XXXXXXX,    XXXXXXX,
+               _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
